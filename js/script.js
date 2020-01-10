359099558c770541f27316a7fcf5b6b0fff8d3f3
Vaibhav Nagvenkar <cool.vaibhav15@gmail.com>
2020-01-10 16:25:19 +0530

    Registration, Login & Logout completed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
 $(document).ready(function(){
    $("#btnsubmit").click(function(e){
          e.preventDefault();
-        //Email validation
-        var res = '<?php echo $rows;';
-        if(res=='0')
-        {
-            $(".reg-empty").addClass("d-block");
-            $(".reg-empty").removeClass("d-none");
-        }
+
         if($("#reg_email_id").val()== ''){
             $(".email-empty").addClass("d-block");
             $(".email-empty").removeClass("d-none");
@@ -17,24 +11,24 @@ $(document).ready(function(){
             $(".email-empty").addClass("d-none");
         }
         //Email validation
-        //Password validation
-        if($("#password").val()== ''){
+        //user_password_one validation
+        if($("#user_password_one").val()== ''){
             $(".pass-empty").addClass("d-block");
             $(".pass-empty").removeClass("d-none");
         }
-        else if($("#password").val()!= ''){
+        else if($("#user_password_one_one").val()!= ''){
             $(".pass-empty").removeClass("d-block");
             $(".pass-empty").addClass("d-none");
         }
-        else if($("#password").val()== '' && $("#user_password_two").val()== '')
+        else if($("#user_password_one").val()== '' && $("#user_password_two").val()== '')
         {
             $(".pass-empty").addClass("d-block");
             $(".pass-empty").removeClass("d-none");
             $(".passtwo-empty").addClass("d-block");
             $(".passtwo-empty").removeClass("d-none");
         }
-        //Password validation
-        //confirm password validation
+        //user_password_one validation
+        //confirm user_password_one validation
         if($("#user_password_two").val()== ''){
             $(".passtwo-empty").addClass("d-block");
             $(".passtwo-empty").removeClass("d-none");
@@ -43,33 +37,79 @@ $(document).ready(function(){
             $(".passtwo-empty").removeClass("d-block");
             $(".passtwo-empty").addClass("d-none");
         }
-        if($("#password").val() != $("#user_password_two").val()){
+        if($("#user_password_one").val() != $("#user_password_two").val()){
             $(".pass-error").addClass("d-block");
             $(".pass-error").removeClass("d-none");
         }
-        else if($("#password").val() == $("#user_password_two").val())
+        else if($("#user_password_one").val() == $("#user_password_two").val())
         {
             $(".pass-error").removeClass("d-block");
             $(".pass-error").addClass("d-none");
-        // }
-        // //confirm password validation
-        // else{
-            
-            
-            var form = new FormData();
-            form.append("reg_email_id", $("#reg_email_id").val());
-            form.append("password",  $("#password").val());
-            $.ajax({
 
-              "url": "http://localhost/Project1/register.php",
-              "method": "POST",
-              "data": form,
-              processData: false,
-              contentType: false
-            }).done(function (response) {
-              console.log(data);
-            });
-        }
+        var form = new FormData();
+        form.append("reg_email_id", $("#reg_email_id").val());
+        form.append("user_password_two",  $("#user_password_two").val());
+        $.ajax({
+
+          "url": "http://localhost/Project1/register.php",
+          "method": "POST",
+          "data": form,
+          processData: false,
+          contentType: false
+        }).done(function (response) {
+            console.log(response)
+          if (response==1) {
+            window.location.replace("http://localhost/Project1/index.php");
+            $(".reg-success").removeClass("d-none");
+            $(".reg-success").addClass("d-block");
+
+            $(".reg-empty").removeClass("d-block");
+            $(".reg-empty").addClass("d-none");
+            
+          }
+          if (response==0) {
+            $(".reg-empty").addClass("d-block");
+            $(".reg-empty").removeClass("d-none");
+          }
+        });
+    }
 });
+
+   $("#loginpage").click(function(e){
+        e.preventDefault();
+        var form = new FormData();
+        form.append("login_email_id", $("#login_email_id").val());
+        form.append("login_user_password",  $("#login_user_password").val());
+        $.ajax({
+
+          "url": "http://localhost/Project1/login.php",
+          "method": "POST",
+          "data": form,
+          processData: false,
+          contentType: false
+        }).done(function (response) {
+            console.log(response);
+          if (response==0) {
+            window.location.replace("http://localhost/Project1/index.php");
+            console.log("Login successful");
+            $(".succ_login").addClass("d-block");
+            $(".succ_login").removeClass("d-none");
+
+            $(".unsucc_login").removeClass("d-block");
+            $(".unsucc_login").addClass("d-none");
+          }
+          if (response==1) {
+            console.log("Register first");
+            $(".unsucc_login").addClass("d-block");
+            $(".unsucc_login").removeClass("d-none");
+          }
+        });
+    });
+
+$("#logout_session").click(function(e){
+    window.location.replace("http://localhost/Project1/logout.php");
+});
+
+
 });
 
--- submit.php
+++ submit.php
@@ -1,5 +1,4 @@
 <?php
-include 'dbconnect.php';
-$sql = "INSERT INTO regusers (email, password) VALUES ('".$_POST["reg_email_id"]."','".$_POST["user_password_one"]."')";
-$result = $conn->query($sql);
-$conn->close();
+session_start();
+$_SESSION['user'] = "test";
+?>
